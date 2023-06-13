3ff89414404320240c22b5a2707a8d28fdf13f6d
18521212 <18521212@gm.uit.edu.vn>
2023-06-13 12:54:43 +0700

    #72 design giao dien chuc nang tao lich hen kham benh

--- reactjs/src/containers/Auth/Login.js
+++ reactjs/src/containers/Auth/Login.js
@@ -63,6 +63,12 @@ class Login extends Component {
         })
     }
 
+    handleKeyDown = (event) => {
+        if (event.key === 'Enter' || event.keyCode === 13) {
+            this.handleLogin();
+        }
+    }
+
     render() {
         return (
             <div className="login-background">
@@ -87,6 +93,7 @@ class Login extends Component {
                                     className='form-control'
                                     placeholder='Enter your password'
                                     onChange={(event) => this.handleOnChangePassword(event)}
+                                    onKeyDown={(event) => this.handleKeyDown(event)}
                                 />
                                 <span onClick={() => { this.handleShowHidePassword() }}>
                                     <i className={this.state.isShowPassword ? 'far fa-eye' : 'far fa-eye-slash'}></i>
