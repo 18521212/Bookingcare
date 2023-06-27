ea46182d567c8df2cec7a77e04db021c0fa389b9
18521212 <18521212@gm.uit.edu.vn>
2023-06-27 23:32:04 +0700

    #77 hoan thien component chon lich kham benh

--- reactjs/src/containers/System/Doctor/ManageSchedule.js
+++ reactjs/src/containers/System/Doctor/ManageSchedule.js
@@ -133,13 +133,20 @@ class ManageSchedule extends Component {
             doctorId: selectedDoctor.value,
             formatedDate: formatedDate
         });
-        console.log('result: ', result)
+
+        if (res && res.errCode === 0) {
+            toast.success('Save infor succeed');
+        } else {
+            toast.error('error saveBulkScheduleDoctor');
+        }
     }
 
     render() {
         console.log('check state: ', this.state)
         let { rangeTime } = this.state;
         let { language } = this.props;
+        let yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
+
         return (
             <div className='manage-schedule-container'>
                 <div className='m-s-tile'>
@@ -161,7 +168,7 @@ class ManageSchedule extends Component {
                                 onChange={this.handleOnchangeDatePicker}
                                 className='form-control'
                                 value={this.state.currentDate}
-                                minDate={new Date()}
+                                minDate={yesterday}
                             />
                         </div>
                         <div className='col-12 pick-hour-container'>
