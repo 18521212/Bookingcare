c4acaf0c03eb5a7cd63691feb57f1e3423c61aa3
18521212 <18521212@gm.uit.edu.vn>
2023-07-11 22:22:52 +0700

    #89 [Check get param value get method] Hoàn thiện chức năng xác nhận lịch hẹn khám bệnh qua email

--- nodejs/src/route/web.js
+++ nodejs/src/route/web.js
@@ -32,7 +32,7 @@ let initWebRoutes = (app) => {
     router.get('/api/get-profile-doctor-by-id', doctorController.getProfileDoctorById);
 
     router.post('/api/patient-book-appointment', patientController.postBookAppointment);
-    router.post('/api/verity-book-appointment', patientController.postVerifyBookAppointment);
+    router.post('/api/verify-book-appointment', patientController.postVerifyBookAppointment);
 
     return app.use("/", router)
 }
