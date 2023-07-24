4bdd8db7aa686d90e668a77c69b5304b33ac7780
18521212 <18521212@gm.uit.edu.vn>
2023-07-24 12:36:06 +0700

    #98 Tạo base giao diện quản lý danh sách lịch hẹn khám bệnh cho bác sĩ

--- reactjs/src/containers/Header/menuApp.js
+++ reactjs/src/containers/Header/menuApp.js
@@ -56,7 +56,10 @@ export const doctorMenu = [
         menus: [
             { //quản lý kế hoạch khám bệnh bác sĩ
                 name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
-            }
+            },
+            { //quản lý bệnh nhân khám bệnh của bác sĩ
+                name: 'menu.doctor.manage-patient', link: '/doctor/manage-patient'
+            },
         ]
     }
 ];
