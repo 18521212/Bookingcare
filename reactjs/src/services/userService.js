2b096ed588f93bd4ee4dbd932326705d09cb77ea
18521212 <18521212@gm.uit.edu.vn>
2023-07-25 21:55:28 +0700

    #99 Hiển thị danh sách bệnh nhân đặt lịch khám bệnh cho bác sĩ

--- reactjs/src/services/userService.js
+++ reactjs/src/services/userService.js
@@ -93,6 +93,10 @@ const createNewClinic = (data) => {
     return axios.post('/api/create-new-clinic', data)
 }
 
+const getAllPatientForDoctor = (data) => {
+    return axios.get(`/api/get-list-patient-for-doctor?doctorId=${data.doctorId}&date=${data.date}`)
+}
+
 
 export {
     handleLoginApi, getAllUsers,
@@ -106,4 +110,5 @@ export {
     getAllSpecialty, getAllDetailSpecialtyById,
     createNewClinic,
     getAllClinic, getAllDetailClinicById,
+    getAllPatientForDoctor,
 }
